972918c597dbab7aceff1cb4211ecf5cea4d8789
Manuel M T Chakravarty <chak@justtesting.org>
2020-10-12 18:30:45 +0200

    CodeEditor: editing support

--- Sources/CodeEditorView/LineMap.swift
+++ Sources/CodeEditorView/LineMap.swift
@@ -30,28 +30,7 @@ struct LineMap<LineInfo> {
 
   /// Initialise a line map with the string to be mapped.
   ///
-  init(string: String) {
-    let nsString = string as NSString
-
-    // Enumerate over all lines in `string`, adding them to the `map`.
-    //
-    var currentIndex = 0
-    while currentIndex < nsString.length {
-
-      let currentRange = nsString.lineRange(for: NSRange(location: currentIndex, length: 0))
-      lines.append((range: currentRange, info: nil))
-      currentIndex = NSMaxRange(currentRange)
-
-    }
-
-    // Check if there is an empty last line (due to a linebreak being at the end of the text), and if so, add that
-    // extra empty line to the map.
-    //
-    let lastRange = nsString.lineRange(for: NSRange(location: nsString.length, length: 0))
-    if lastRange.length == 0 {
-      lines.append((range: lastRange, info: nil))
-    }
-  }
+  init(string: String) { lines.append(contentsOf: linesOf(string: string)) }
 
   /// MARK: -
   /// MARK: Queries
@@ -69,7 +48,7 @@ struct LineMap<LineInfo> {
   /// - Parameter index: The string index of the characters whose line we want to determine.
   /// - Returns: The line containing the indexed character if the index is within the bounds of the string.
   ///
-  /// This functions asymptotic complexity is logarithmic in the number of lines contained in the line map.
+  /// - Complexity: This functions asymptotic complexity is logarithmic in the number of lines contained in the line map.
   ///
   func lineContaining(index: Int) -> Int? {
     var lineRange = 1..<lines.count
@@ -98,6 +77,65 @@ struct LineMap<LineInfo> {
     }
   }
 
+  /// Determine the line that contains the cursor position specified by the given string index. (Safe to be called with
+  /// an out of bounds index.)
+  ///
+  /// Corresponds to `lineContaining(index:)`, but also handles the index just after the last valid string index — i.e.,
+  /// the end-of-string insertion point.
+  ///
+  /// - Parameter index: The string index of the cursor position whose line we want to determine.
+  /// - Returns: The line containing the given cursor poisition if the index is within the bounds of the string or
+  ///            just beyond.
+  ///
+  /// - Complexity: This functions asymptotic complexity is logarithmic in the number of lines contained in the line
+  ///               map.
+  ///
+  func lineOf(index: Int) -> Int? {
+    if let lastLine = lines.last, NSMaxRange(lastLine.range) == index { return lines.count - 1 }
+    else { return lineContaining(index: index) }
+  }
+
+  /// Given a character range, return the smallest line range that includes the characters plus maybe a trailing empty
+  /// line. Deal with out of bounds conditions by clipping to the front and end of the line range, respectively.
+  ///
+  /// - Parameter range: The character range for which we want to know the line range.
+  /// - Returns: The smallest range of lines that includes all characters in the given character range.
+  ///
+  /// There are two special cases:
+  /// - If the resulting line range is being followed by a trailing empty line, that trailing empty line is also
+  ///   included in the result.
+  /// - If the character range is of length zero, we return the line of the start location. We do that also if the start
+  ///   location is just behind the last character of the text.
+  ///
+  func linesContaining(range: NSRange) -> Range<Int> {
+    let
+      start       = range.location < 0 ? 0 : range.location,
+      end         = range.length <= 0 ? start : NSMaxRange(range) - 1,
+      startLine   = lineOf(index: start),
+      endLine     = lineContaining(index: end),
+      lastLine    = lines.count - 1,
+      realEndLine : Int?
+
+    if let endLine = endLine,
+       endLine + 1 == lastLine,                                 // 'endLine' is right before the 'lastLine'
+       lines[lastLine].range.length == 0                        // 'lastLine' is an empty line
+    {
+
+      realEndLine = lastLine                                    // extend 'endLine' to 'lastLine'
+
+    } else { realEndLine = endLine }
+
+    if let startLine = startLine {
+
+      if range.length < 0 { return startLine..<startLine } else { return Range<Int>(startLine...(realEndLine ?? lastLine)) }
+
+    } else {
+
+      if range.location < 0 { return 0..<0 } else { return lastLine..<lastLine }
+
+    }
+  }
+
   /// MARK: -
   /// MARK: Editing
 
@@ -108,9 +146,89 @@ struct LineMap<LineInfo> {
   ///   - editedRange: The character range that was affected by editing (after editing).
   ///   - delta: The length increase of the edited string (negative if it got shorter).
   ///
+  /// NB: The line after the `editedRange` will be updated (and info fields be invalidated) if the `editedRange` ends on
+  ///     a newline.
+  ///
   mutating func updateAfterEditing(string: String, range editedRange: NSRange, changeInLength delta: Int) {
 
+    // If the given ranges are out of bounds of the given string, just recompute the complete line map.
+    //
+    let maxEditedRange = NSMaxRange(editedRange)
+    if editedRange.location < 0 || editedRange.length < 0 || editedRange.length - delta < 0
+        || maxEditedRange > string.count
+        || maxEditedRange - delta > NSMaxRange(lines.last?.range ?? NSRange(location: 0, length: 0))
+    {
+
+      lines = [(range: NSRange(location: 0, length: 0), info: nil)] + linesOf(string: string)
+      return
+
+    }
+
+    // Unless the edited range reaches to the end of the string, extend it by one character. This is crucial as, if the
+    // edited range ends on a newline, this may insert a new line break, which means, we also need to update the line
+    // *after* the new line break.
+    let extendedEditedRange
+      = maxEditedRange == string.count
+      ? editedRange
+      : NSRange(location: editedRange.location, length: editedRange.length + 1)
+
+    let
+      nsString       = string as NSString,
+      oldLinesRange  = linesContaining(range: NSRange(location: extendedEditedRange.location,
+                                                      length: extendedEditedRange.length - delta)),
+      newLinesRange  = nsString.lineRange(for: extendedEditedRange),
+      newLinesString = nsString.substring(with: newLinesRange),
+      newLines       = linesOf(string: newLinesString).map{ shift(line: $0, by: newLinesRange.location) }
+
+    // If the newly inserted text ends on a new line, we need to remove the empty trailing line in the new lines array
+    // unless the range of those lines extends until the end of the string.
+    let dropEmptyNewLine = newLines.last?.range.length == 0 && NSMaxRange(newLinesRange) < string.count,
+        adjustedNewLines = dropEmptyNewLine ? newLines.dropLast() : newLines
+
+    lines.replaceSubrange(oldLinesRange, with: adjustedNewLines)
+
+    // All ranges after the edited range of lines need to be adjusted.
+    //
+    for i in oldLinesRange.startIndex.advanced(by: adjustedNewLines.count) ..< lines.count {
+      lines[i] = shift(line: lines[i], by: delta)
+    }
   }
 
-}
+  /// MARK: -
+  /// MARK: Helpers
 
+  /// Shift the range of `line` by `delta`.
+  ///
+  private func shift(line: OneLine, by delta: Int) -> OneLine {
+    return (range: NSRange(location: line.range.location + delta, length: line.range.length), info: line.info)
+  }
+
+  /// Extract the corresponding array of line ranges out of the given string.
+  ///
+  private func linesOf(string: String) -> [OneLine] {
+    let nsString = string as NSString
+
+    var resultingLines: [OneLine] = []
+
+    // Enumerate all lines in `nsString`, adding them to the `resultingLines`.
+    //
+    var currentIndex = 0
+    while currentIndex < nsString.length {
+
+      let currentRange = nsString.lineRange(for: NSRange(location: currentIndex, length: 0))
+      resultingLines.append((range: currentRange, info: nil))
+      currentIndex = NSMaxRange(currentRange)
+
+    }
+
+    // Check if there is an empty last line (due to a linebreak being at the end of the text), and if so, add that
+    // extra empty line to the `resultingLines` as well.
+    //
+    let lastRange = nsString.lineRange(for: NSRange(location: nsString.length, length: 0))
+    if lastRange.length == 0 {
+      resultingLines.append((range: lastRange, info: nil))
+    }
+
+    return resultingLines
+  }
+}
--- Tests/CodeEditorTests/LineMapTest.swift
+++ Tests/CodeEditorTests/LineMapTest.swift
@@ -9,49 +9,49 @@ final class LineMapTests: XCTestCase {
 
   // Initialisation tests
 
-  func testInit(_ string: String, _ lineMap: LineMap<Void>) {
+  func hasLineMap(_ string: String, _ lineMap: LineMap<Void>) {
     let computedLineMap = LineMap<Void>(string: string)
     XCTAssertEqual(computedLineMap.lines.map{ $0.range }, lineMap.lines.map{ $0.range })
   }
 
   func testInitEmpty() {
-    testInit("",
-             LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 0)]))
+    hasLineMap("",
+               LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 0)]))
   }
 
   func testInitLineBreak() {
-    testInit("\n",
-             LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 1), mkRange(loc: 1, len: 0)]))
+    hasLineMap("\n",
+               LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 1), mkRange(loc: 1, len: 0)]))
   }
 
   func testInitSimple() {
-    testInit("abc",
-             LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 3)]))
+    hasLineMap("abc",
+               LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 3)]))
   }
 
-  func testInitEmptyTrailing() {
-    testInit("abc\n",
-             LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 4), mkRange(loc: 4, len: 0)]))
+  func testInitSimpleTrailing() {
+    hasLineMap("abc\n",
+               LineMap<Void>(lines: [mkRange(loc: 0, len: 0), mkRange(loc: 0, len: 4), mkRange(loc: 4, len: 0)]))
   }
 
   func testInitLines() {
-    testInit("abc\ndefg\nhij",
-             LineMap<Void>(lines: [mkRange(loc: 0, len: 0),
-                                   mkRange(loc: 0, len: 4),
-                                   mkRange(loc: 4, len: 5),
-                                   mkRange(loc: 9, len: 3)]))
+    hasLineMap("abc\ndefg\nhij",
+               LineMap<Void>(lines: [mkRange(loc: 0, len: 0),
+                                     mkRange(loc: 0, len: 4),
+                                     mkRange(loc: 4, len: 5),
+                                     mkRange(loc: 9, len: 3)]))
   }
 
   func testInitEmptyLines() {
-    testInit("\nabc\n\n\ndefg\nhi\n",
-             LineMap<Void>(lines: [mkRange(loc: 0, len: 0),
-                                   mkRange(loc: 0, len: 1),
-                                   mkRange(loc: 1, len: 4),
-                                   mkRange(loc: 5, len: 1),
-                                   mkRange(loc: 6, len: 1),
-                                   mkRange(loc: 7, len: 5),
-                                   mkRange(loc: 12, len: 3),
-                                   mkRange(loc: 15, len: 0)]))
+    hasLineMap("\nabc\n\n\ndefg\nhi\n",
+               LineMap<Void>(lines: [mkRange(loc: 0, len: 0),
+                                     mkRange(loc: 0, len: 1),
+                                     mkRange(loc: 1, len: 4),
+                                     mkRange(loc: 5, len: 1),
+                                     mkRange(loc: 6, len: 1),
+                                     mkRange(loc: 7, len: 5),
+                                     mkRange(loc: 12, len: 3),
+                                     mkRange(loc: 15, len: 0)]))
   }
 
 
@@ -74,7 +74,7 @@ final class LineMapTests: XCTestCase {
     XCTAssertEqual(lineMap.lineContaining(index: 2), 1)
   }
 
-  func testLookupEmptyTrailing() {
+  func testLookupSimpleTrailing() {
     let lineMap = LineMap<Void>(string: "abc\n")
     XCTAssertNil(lineMap.lineContaining(index: 4))
     XCTAssertEqual(lineMap.lineContaining(index: 0), 1)
@@ -99,14 +99,99 @@ final class LineMapTests: XCTestCase {
     XCTAssertEqual(lineMap.lineContaining(index: 14), 6)
   }
 
+  // Editing tests
+
+  func testEditing(string: String, into newString: String, range: NSRange, changeInLength: Int) {
+    var lineMap = LineMap<Void>(string: string)
+    lineMap.updateAfterEditing(string: newString, range: range, changeInLength: changeInLength)
+    hasLineMap(newString, lineMap)
+  }
+
+  func testEditingEmpty() {
+    let string = ""
+    testEditing(string: string, into: "abc", range: NSRange(location: 0, length: 3), changeInLength: 3)
+    testEditing(string: string, into: "abc\n", range: NSRange(location: 0, length: 4), changeInLength: 4)
+  }
+
+  func testEditingLineBreak() {
+    let string = "\n"
+    testEditing(string: string, into: "abc", range: NSRange(location: 0, length: 3), changeInLength: 2)
+    testEditing(string: string, into: "abc\n", range: NSRange(location: 0, length: 3), changeInLength: 3)
+    testEditing(string: string, into: "\nabc", range: NSRange(location: 1, length: 3), changeInLength: 3)
+    testEditing(string: string, into: "\n\n", range: NSRange(location: 0, length: 1), changeInLength: 1)
+
+    testEditing(string: string, into: "x", range: NSRange(location: 0, length: 1), changeInLength: 0)
+
+    testEditing(string: string, into: "", range: NSRange(location: 0, length: 0), changeInLength: -1)
+  }
+
+  func testEditingSimple() {
+    let string = "abc"
+    testEditing(string: string, into: "abc", range: NSRange(location: 1, length: 0), changeInLength: 0)
+    testEditing(string: string, into: "abc\n", range: NSRange(location: 3, length: 1), changeInLength: 1)
+    testEditing(string: string, into: "\nabc", range: NSRange(location: 0, length: 1), changeInLength: 1)
+    testEditing(string: string, into: "ab\nc", range: NSRange(location: 2, length: 1), changeInLength: 1)
+    testEditing(string: string, into: "ab\n\n\nc", range: NSRange(location: 2, length: 3), changeInLength: 3)
+
+    testEditing(string: string, into: "ac", range: NSRange(location: 1, length: 0), changeInLength: -1)
+  }
+
+  func testEditingSimpleTrailing() {
+    let string = "abc\n"
+    testEditing(string: string, into: "abc\n\n\n", range: NSRange(location: 4, length: 2), changeInLength: 2)
+    testEditing(string: string, into: "abc\n\n", range: NSRange(location: 3, length: 1), changeInLength: 1)
+    testEditing(string: string, into: "ab\n\n\nc\n", range: NSRange(location: 2, length: 3), changeInLength: 3)
+
+    testEditing(string: string, into: "abcx", range: NSRange(location: 3, length: 1), changeInLength: 0)
+
+    testEditing(string: string, into: "abc", range: NSRange(location: 3, length: 0), changeInLength: -1)
+  }
+
+  func testEditingEmptyLines() {
+    let string = "\nabc\n\n\ndefg\nhi\n"
+    testEditing(string: string, into: "x\nabc\n\n\ndefg\nhi\n", range: NSRange(location: 0, length: 1), changeInLength: 1)
+    testEditing(string: string, into: "\nxabc\n\n\ndefg\nhi\n", range: NSRange(location: 1, length: 1), changeInLength: 1)
+    testEditing(string: string, into: "\nabcx\n\n\ndefg\nhi\n", range: NSRange(location: 4, length: 1), changeInLength: 1)
+    testEditing(string: string, into: "\nabc\nx\n\ndefg\nhi\n", range: NSRange(location: 5, length: 1), changeInLength: 1)
+
+    testEditing(string: string, into: "\nabc\n\n\ndefg\nhix", range: NSRange(location: 14, length: 1), changeInLength: 0)
+    testEditing(string: string, into: "\nabc\nx\ndefg\nhi\n", range: NSRange(location: 5, length: 1), changeInLength: 0)
+
+    testEditing(string: string, into: "\nabc\n\ndefg\nhi\n", range: NSRange(location: 5, length: 0), changeInLength: -1)
+    testEditing(string: string, into: "\nabc\n\ndefg\nhi\n", range: NSRange(location: 6, length: 0), changeInLength: -1)
+    testEditing(string: string, into: "\nabcdefg\nhi\n", range: NSRange(location: 4, length: 0), changeInLength: -3)
+    testEditing(string: string, into: "abc\n\n\ndefg\nhi\n", range: NSRange(location: 0, length: 0), changeInLength: -1)
+  }
+
+  func testEditingBogus() {
+    let string = "\nabc\n\n\ndefg\nhi\n"
+
+    testEditing(string: string, into: "\nabc\nx\ndefg\nhi\n", range: NSRange(location: 5, length: 1), changeInLength: -100)
+    testEditing(string: string, into: "\nabc\nx\ndefg\nhi\n", range: NSRange(location: 5, length: 1), changeInLength: 10)
+    testEditing(string: string, into: "\nabc\nx\ndefg\nhi\n", range: NSRange(location: 5, length: 20), changeInLength: 10)
+  }
+
   static var allTests = [
     ("testInitEmpty", testInitEmpty),
     ("testInitLineBreak", testInitLineBreak),
     ("testInitSimple", testInitSimple),
-    ("testInitEmptyTrailing", testInitEmptyTrailing),
+    ("testInitEmptyTrailing", testInitSimpleTrailing),
     ("testInitLines", testInitLines),
     ("testInitEmptyLines", testInitEmptyLines),
 
-    ("textLookupEmpty", testLookupEmpty),
+    ("testLookupEmpty", testLookupEmpty),
+    ("testLookupLineBreak", testLookupLineBreak),
+    ("testLookupSimple", testLookupSimple),
+    ("testLookupEmptyTrailing", testLookupSimpleTrailing),
+    ("testLookupLines", testLookupLines),
+    ("testLookupEmptyLines", testLookupEmptyLines),
+
+    ("testEditingEmpty", testEditingEmpty),
+    ("testEditingLineBreak", testEditingLineBreak),
+    ("testEditingSimple", testEditingSimple),
+    ("testEditingSimpleTrailing", testEditingSimpleTrailing),
+    ("testEditingEmptyLines", testEditingEmptyLines),
+
+    ("testEditingBogus", testEditingBogus),
   ]
 }
