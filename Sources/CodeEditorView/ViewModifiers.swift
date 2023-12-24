2a4fa21a1a678faf4578b4f2bd2abde134aebf38
Manuel M T Chakravarty <chak@justtesting.org>
2023-12-24 13:59:27 +0100

    Add onRotate modifier for iOS

--- Sources/CodeEditorView/ViewModifiers.swift
+++ Sources/CodeEditorView/ViewModifiers.swift
@@ -56,3 +56,30 @@ extension View {
   }
 }
 
+
+#if os(iOS)
+
+// MARK: -
+// MARK: Dedecting device rotation
+
+fileprivate struct DeviceRotationViewModifier: ViewModifier {
+  let action: (UIDeviceOrientation) -> Void
+
+  func body(content: Content) -> some View {
+    content
+      .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+        action(UIDevice.current.orientation)
+      }
+  }
+}
+
+extension View {
+  
+  /// Perform an action every time the device rotates.
+  ///
+  func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+    self.modifier(DeviceRotationViewModifier(action: action))
+  }
+}
+
+#endif
