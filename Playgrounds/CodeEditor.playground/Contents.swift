2f32ef01b2601527d0c7ad06d04595987827a22a
Kwok <31276761+KwokZL@users.noreply.github.com>
2025-08-25 20:22:14 +0800

    Update playground file

--- Playgrounds/CodeEditor.playground/Contents.swift
+++ Playgrounds/CodeEditor.playground/Contents.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 import CodeEditorView
+import LanguageSupport
 
 
 struct Editor: View {
   @State private var text = "main = print 42"
+  @State private var position: CodeEditor.Position       = CodeEditor.Position()
+  @State private var messages: Set<TextLocated<Message>> = Set()
 
   var body: some View {
-    CodeEditor(text: $text)
+      CodeEditor(text: $text, position: $position, messages: $messages, language: .swift())
   }
 }
 
